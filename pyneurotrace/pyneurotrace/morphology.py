4a7b081f5207e2f96354b566783aed297df68e95
Pat Coleman <padsterpat@gmail.com>
2018-09-18 14:58:44 -0700

    Added treeToFiloTipAndBase, finding Filo POI in a tree

--- pyneurotrace/pyneurotrace/morphology.py
+++ pyneurotrace/pyneurotrace/morphology.py
@@ -46,3 +46,42 @@ def _treeSize(nodeID, nodes):
     for child in nodes[nodeID]['children']:
         size += _treeSize(child['id'], nodes)
     return size
+
+# Return dictionary mapping: node ID -> (branchID if filo tip, -branchID if filo base, or 0 if neither)
+def treeToFiloTipAndBase(nodeIDs, nodeXYZ, tree, rootID, filoDist=5.0):
+    mapping = {}
+    nextBranchID = [0]
+    
+    # Inner function to process a single node, given where it started and the distance to there.
+    def _filoInner(nodeAtID, branchStartID, branchDist):
+        # Default to nothing, change later if needed.
+        mapping[nodeAtID] = 0
+        
+        nChildren = len(tree[nodeAtID]['children'])
+        nodeAtIdx = nodeIDs.index(nodeAtID) 
+        pAt = nodeXYZ[nodeAtIdx]
+
+        # middle of branch, so just keep going:
+        if nChildren == 1:
+            nextNodeID = tree[nodeAtID]['children'][0]['id']
+            pNext = nodeXYZ[nodeIDs.index(nextNodeID)]
+            dist = np.linalg.norm(pNext - pAt)
+            _filoInner(nextNodeID, branchStartID, branchDist + dist)
+        # Child branches, so clear and start new below
+        elif nChildren > 1:
+            for child in tree[nodeAtID]['children']:
+                pNext = nodeXYZ[nodeIDs.index(child['id'])]
+                dist = np.linalg.norm(pNext - pAt)
+                # Note: Base is the first in the filo, not the parent as it may have multiple.
+                _filoInner(child['id'], child['id'], dist)
+        # Filopodia tip (scale from m to um)
+        elif (branchDist * 1e6) <= filoDist:
+            mapping[branchStartID] = -nextBranchID[0]
+            mapping[nodeAtID] = nextBranchID[0]
+            nextBranchID[0] += 1
+        # Branch tip (ignore)
+        else:
+            pass
+        
+    _filoInner(rootID, rootID, 0)
+    return mapping
