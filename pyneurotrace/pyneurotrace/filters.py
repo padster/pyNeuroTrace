5f523fdb7d33b5ce3122560c67cd7754f51900e5
padster <padsterpat@gmail.com>
2019-08-07 14:54:31 -0700

    Cleaned up NND smoothing

--- pyneurotrace/pyneurotrace/filters.py
+++ pyneurotrace/pyneurotrace/filters.py
@@ -2,6 +2,8 @@
 import math
 import numpy as np
 
+from .nndFilter import nonNegativeDeconvolution as NND
+
 """
 Friedrich, J., Zhou, P., & Paninski, L. (2017).
 Fast online deconvolution of calcium imaging data.
@@ -15,6 +17,40 @@ def oasisSmooth(data, iterFunc=None):
         #clean, _, _, _, _ = oasisDeconvolve(samples, g=(None, None), optimize_g=5)
         return clean
     return _forEachTimeseries(data, _singleRowOasis, iterFunc)
+
+
+"""
+Podgorski, K., & Haas, K. (2013).
+Fast non‐negative temporal deconvolution for laser scanning microscopy.
+Journal of biophotonics, 6(2), 153-162.
+"""
+def nndSmooth(data, hz, tau, iterFunc=None):
+    tauSamples = tau * hz
+
+    # This is the transient shape we're deconvolving against:
+    # e^(x/tauSamples), for 8 times the length of tau.
+    cutoff = round(8 * tauSamples)
+    fitted = np.exp(-np.arange(cutoff + 1) / tauSamples)
+
+    def _singleRowNND(samples):
+        result = np.copy(samples)
+        nanSamples = np.isnan(samples)
+        if np.all(nanSamples):
+            pass # No data
+        elif not np.any(nanSamples):
+            # All samples exist, so fit in one go
+            result = np.convolve(NND(samples, tauSamples), fitted)[:len(samples)]
+        else:
+            # Lots of different runs of samples, fit each separately
+            starts = np.where((not nanSamples) & np.isnan(np.concatenate(([1], samples[:-1]))))[0]
+            ends = np.where((not nanSamples) & np.isnan(np.concatenate((samples[1:], [1]))))[0]
+            for start, end in zip(starts, ends):
+                tmp = np.convolve(NND(samples[start:end], tauSamples), fitted)
+                result[start:end] = np.max(0, tmp[:end - start + 1])
+        return result
+
+    return _forEachTimeseries(data, _singleRowNND, iterFunc)
+
 
 """
 Okada, M., Ishikawa, T., & Ikegaya, Y. (2016).
@@ -85,129 +121,3 @@ def _forEachTimeseries(data, func, iterFunc=None):
             for j in iterFunc(range(data.shape[1])):
                 result[i, j] = func(data[i, j])
     return result
-    
-    
-"""
-TODO : Clean up
-"""
-# performs fast nonnegative deconvolution on pmt signal to solve for minimum MSE photon rate
-#   trace  :   The data to be deconvolved
-#   tau    :   The time constant of the PMT, in data samples
-#   return :   estimated photon rate
-def NN_KP(trace, tau):
-    T = len(trace)
-    counts = np.zeros(T)
-    counts[-1] = trace[-1]
-    cutoff = math.ceil(8 * tau)
-    kernel = np.exp(-np.arange(cutoff + 1)/tau) # convolution kernel
-    recent = np.full(1 + round(T / 2), np.nan).astype(int)
-    recent[0] = T #stored locations where we assigned counts
-    recentIdx = 0
-   
-    # the points that could potentially be assigned counts:
-    _delayed = np.concatenate(([0], trace[:-2]))
-    points = (trace[:-1] > kernel[1] * _delayed) & (trace[:-1] > 0)
-    
-    # dividing these points up into runs, for speed
-    runStarts = np.where(points & ~(np.concatenate(([False], points[:-1]))))[0].astype(int)
-    runEnds = np.where(points & ~(np.concatenate((points[1:], [False]))))[0].astype(int)
-    runIdx = len(runEnds) - 1
-    
-    while runIdx >= 0:
-        oldTop, oldBottom = 0, 0
-        t = runEnds[runIdx]
-        t1 = t
-        accum = 0
-        
-        converged = False
-        while not converged:
-            if recentIdx >= 0 and recent[recentIdx] < (t+cutoff):
-                t2 = recent[recentIdx] - 1
-                C_max = counts[t2] / kernel[t2-t]
-            else:
-                t2 = min(t + cutoff, T+1) - 1
-                C_max = np.inf
-            
-
-            b = kernel[t1-t:t2-t]
-            top = np.dot(b, trace[t1:t2]) + oldTop #this is the numerator of the least squares fit for an exponential
-            bottom = np.dot(b, b) + oldBottom #this is the denominator of the fit
-            
-            done = False
-            while not done:
-                #the error function is (data-kernel.*C)^2
-                bestC = max(top/bottom, 0);  #C=top/bottom sets the derivative of the error to 0
-
-                # does not meet nonnegative constraint. Continue to adjust previous solutions.
-                if bestC > (C_max+accum): 
-                    accum = accum + counts[t2] / kernel[t2-t]
-                    counts[t2] = 0
-                    t1 = t2
-                    oldTop = top
-                    oldBottom = bottom
-                    recentIdx -= 1
-                    done = True
-                    
-                else: # converged!
-                    #now that we have found the MSE counts for times t<end, check if
-                    #this will be swamped by the next timepoint in the run
-                    if  (t == runStarts[runIdx]) or (trace[t-1] < bestC/kernel[1]): #%C_max won't necessarily get swamped
-                        if recentIdx >= 0 and t2 <= t + cutoff:
-                            counts[t2] = counts[t2] - (bestC - accum) * kernel[t2-t]
-                        runStart = runStarts[runIdx]
-                        initIdx = recentIdx + 1
-                        recentIdx = recentIdx + 1 + t - runStart;
-                        
-                        _skipped = 0
-                        if recentIdx + 1 > len(recent):
-                            _skipped = recentIdx - (len(recent) - 1)
-                            recentIdx = len(recent) - 1
-                            
-                            
-                        recent[initIdx:recentIdx + 1] = np.arange(t+1, runStart + _skipped, -1)
-                        counts[runStart:(t+1)] = \
-                               np.concatenate((trace[runStart:t], [bestC])) - \
-                               np.concatenate(([0], kernel[1]*trace[runStart:t]))
-                        done = True
-                        converged = True
-                    else: #%C_max will get swamped
-                        #%in this situation, we know that this point will be removed
-                        #%as we continue to process the run. To save time:
-                        t -= 1
-                        runEnds[runIdx] = t
-                        accum = accum / kernel[1]
-                        top = top * kernel[1] + trace[t] #% %this is the correct adjustment to the derivative term above
-                        bottom = bottom * (kernel[1] ** 2) + 1 #% %this is the correct adjustment to the derivative term above
-
-        runIdx -= 1
-    return counts
-
-def filterNND(traces, tau):
-    result = np.copy(traces)
-    
-    cutoff = round(8 * tau)
-    fitted = np.exp(-np.arange(cutoff + 1)/tau)
-    
-    nRows, nSamples = traces.shape
-    for i in range(nRows):
-        trace = traces[i]
-        traceIsNan = np.isnan(trace)
-        
-        if np.all(traceIsNan):
-            continue
-            
-        if not np.any(traceIsNan):
-            result[i] = np.convolve(NN_KP(trace, tau), fitted)[:nSamples]
-        else:
-            starts = np.where((not np.isnan(trace)) & np.isnan(np.concatenate(([1], trace[:-1]))))[0]
-            ends = np.where((not np.isnan(trace)) & np.isnan(np.concatenate((trace[1:], [1]))))[0]
-            for start, end in zip(starts, ends):
-                tmp = np.convolve(NN_KP(trace[start:end], tau), fitted)
-                result[i, start:end] = np.max(0, tmp[:end - start + 1])
-    return result
-
-
-def kpNNDSmooth(data, hz, tau, iterFunc=None):
-    tauSamples = tau * hz
-    return filterNND(data, tauSamples)
-    
